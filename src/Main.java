7b9552fd07c025a5d696b688652e68793a62dc4d
truesik <truesik@gmail.com>
2016-04-19 14:59:32 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
+import light.GreenLight;
+import light.RedLight;
+import light.YellowLight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите время работы зеленого света (в секундах):");
-        Scanner scannerGreen = new Scanner(System.in);
-        int greenDuration = scannerGreen.nextInt();
-
-        System.out.println("Введите время работы желтого света (в секундах):");
-        Scanner scannerYellow = new Scanner(System.in);
-        int yellowDuration = scannerYellow.nextInt();
-
-        System.out.println("Введите время работы красного света (в секундах):");
-        Scanner scannerRed = new Scanner(System.in);
-        int redDuration = scannerRed.nextInt();
-
-        Thread trafficThread = new Thread(new TrafficLight(redDuration, yellowDuration, greenDuration));
-        trafficThread.start();
+        TrafficLight trafficLight = new TrafficLightImp(new GreenLight(), new YellowLight(), new RedLight());
+        TrafficLightController trafficLightController = new TrafficLightController(trafficLight);
+        Duration duration = new DurationReader().read();
+        trafficLightController.run(duration);
     }
 }
