8e0f4246ee25779745de2d7e1711f42924d6cc49
truesik <truesik@gmail.com>
2016-04-19 14:58:59 +0300

    Интерфейс светофора

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,35 +1,7 @@
-import light.GreenLight;
-import light.Light;
-import light.RedLight;
-import light.YellowLight;
+public interface TrafficLight {
+    void turnOnGreenLight(int greenDurationInSeconds);
 
-public class TrafficLight implements Runnable {
-    private Light redLight;
-    private Light yellowLight;
-    private Light greenLight;
+    void turnOnYellowLight(int yellowDurationInSeconds);
 
-    public TrafficLight(int ...durationInSeconds) {
-        redLight = new RedLight(durationInSeconds[0]);
-        yellowLight = new YellowLight(durationInSeconds[1]);
-        greenLight = new GreenLight(durationInSeconds[2]);
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            startAndJoinThread(greenLight);
-            startAndJoinThread(yellowLight);
-            startAndJoinThread(redLight);
-        }
-    }
-
-    private void startAndJoinThread(Light light) {
-        Thread thread = new Thread(light);
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    void turnOnRedLight(int redLightDurationInSeconds);
 }
