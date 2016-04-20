fbab8d34fa101013ea746363861d714824d5a5ea
truesik <truesik@gmail.com>
2016-04-20 22:19:19 +0300

    Исключения

--- src/DurationReader.java
+++ src/DurationReader.java
@@ -15,7 +15,48 @@ public class DurationReader {
     }
 
     private int getInt() {
+        int value;
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        String line = scanner.nextLine();
+        if (isInteger(line)) {
+            if (isPositive(Integer.valueOf(line))) {
+                value = Integer.valueOf(line);
+            } else {
+                System.err.println("Введите положительное число!");
+                return getInt();
+            }
+        } else {
+            System.err.println("Введите число!");
+            return getInt();
+        }
+        return value;
+    }
+
+    private boolean isPositive(int value) {
+        return value >= 0;
+    }
+
+    private boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return false;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 }
